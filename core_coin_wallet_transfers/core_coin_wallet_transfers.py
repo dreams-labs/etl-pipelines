a402f8c3f30bbb3fac84d8fa3e4e52173c66d8e1
tentabs00 <tentabs00@gmail.com>
2024-09-04 20:41:57 -0700

    add exclusions logic

--- core_coin_wallet_transfers/core_coin_wallet_transfers.py
+++ core_coin_wallet_transfers/core_coin_wallet_transfers.py
@@ -21,6 +21,9 @@ def update_core_coin_wallet_transfers(request):
     '''
     runs all functions in sequence to refresh core.coin_market_data
     '''
+    # update list of addresses to be excluded from the core table
+    update_exclusions_table()
+
     # insert new coingecko market data records to core.coin_market_data
     logger.info(f'rebuilding table core.coin_wallet_transfers...')
     counts_df = rebuild_core_coin_wallet_transfers()
@@ -31,6 +34,29 @@ def update_core_coin_wallet_transfers(request):
     logger.info('rebuilt core.coin_wallet_transfers [%s rows] from etl_pipelines.coin_wallet_net_transfers [%s rows].',core_count,etl_count)
 
     return f'{{"rebuild of core.coin_wallet_transfers complete."}}'
+
+
+
+def update_exclusions_table():
+    '''
+    refreshes the etl_pipelines.coin_wallet_net_transfers_exclusions bigquery table by ingesting 
+    the underlying sheets data, formatting it, and uploading it to bigquery
+
+    exclusions are maintained in the 'coin_wallet_net_transfers_exclusions' tab of this google sheet:
+    https://docs.google.com/spreadsheets/d/11Mi1a3SeprY_GU_QGUr_srtd7ry2UrYwoaRImSACjJs/edit?gid=388901135#gid=388901135
+    '''
+    # load the tab into a df
+    df = dgc().read_google_sheet('11Mi1a3SeprY_GU_QGUr_srtd7ry2UrYwoaRImSACjJs','coin_wallet_net_transfers_exclusions!A:E')
+
+    # format and upload df
+    df['created_date'] = pd.to_datetime(df['created_date'])
+    df['updated_at'] = datetime.datetime.now(utc)
+    dgc().upload_df_to_bigquery(
+        df,
+        'etl_pipelines',
+        'coin_wallet_net_transfers_exclusions',
+        if_exists='replace'
+    )
 
 
 
@@ -61,12 +87,25 @@ def rebuild_core_coin_wallet_transfers():
             join core.chains ch on ch.chain_id = c.chain_id
             join etl_pipelines.coin_wallet_net_transfers wnt on wnt.token_address = c.address 
                 and (wnt.chain_text_source = ch.chain_text_dune and wnt.data_source = 'dune')
+            left join (
+                select e.chain_text_source
+                ,case 
+                    when ch.is_case_sensitive=False then lower(e.wallet_address)
+                    else e.wallet_address
+                    end as wallet_address
+                from `etl_pipelines.coin_wallet_net_transfers_exclusions` e
+                join `core.chains` ch on ch.chain_text_dune = e.chain_text_source
+            ) exclusions on exclusions.wallet_address = wnt.wallet_address
+                and exclusions.chain_text_source = wnt.chain_text_source
 
             -- remove dune's various representations of burn/mint addresses
             where wnt.wallet_address <> 'None' -- removes burn/mint address for solana
             and wnt.wallet_address <> '0x0000000000000000000000000000000000000000' -- removes burn/mint addresses
             and wnt.wallet_address <> wnt.token_address 
             and wnt.wallet_address <> '<nil>'
+
+            -- remove manually excluded addresses
+            and exclusions.wallet_address is null
         );
 
         select 'core.coin_wallet_transfers' as table
