e324ab1546219cb6a2f0087594afac40b49b9c53
tentabs00 <tentabs00@gmail.com>
2024-09-27 11:45:13 -0700

    all_coins.py finished v1

--- cloud_functions/coingecko_all_coins/coingecko_all_coins.py
+++ cloud_functions/coingecko_all_coins/coingecko_all_coins.py
@@ -3,7 +3,7 @@ Retrieves all coins from coingecko
 """
 import time
 import datetime
-import logging
+from typing import List, Dict
 import os
 import json
 import requests
@@ -18,137 +18,127 @@ logger = dc.setup_logger()
 
 
 
-# @functions_framework.http
-# def retrieve_coingecko_metadata(request): # pylint: disable=unused-argument  # noqa: F841
-#     '''
-#     Queries BigQuery to obtain a list of coins that need metadata and attempts to match them and
-#     store metadata by calling coingecko_metadata_search() for each.
-#     '''
+@functions_framework.http
+def retrieve_coingecko_all_coins(request): # pylint: disable=unused-argument  # noqa: F841
+    '''
+    Retrieves and uploads a page from the all coins endpoint for the given page range
+    '''
 
-#     # pull list of coins to attempt
-#     query_sql = '''
-#         select cc.coin_id
-#         ,ch.chain_text_coingecko
-#         ,cc.address
-#         from core.coins cc
-#         join core.chains ch on ch.chain_id = cc.chain_id
-#         left join etl_pipelines.coin_coingecko_ids cgi on cgi.coin_id = cc.coin_id
-#             and cgi.search_log = "{'error': 'coin not found'}"
-#         left join etl_pipelines.coin_coingecko_metadata cgm on cgm.coingecko_id = cc.coingecko_id
-#         -- don't include coins without addresses
-#         where cc.address is not null
+    # retrieve the number of pages to upload and store the batch time
+    pages = request.args.get('all_coins_pages', default=1, type=int)
+    batch_datetime = f"{datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')}"
 
-#         -- don't reattempt addresses that couldn't be found
-#         and cgi.coin_id is null
+    # ping api for each coin, adjusting the range so it starts at 1
+    for page in range(1, pages + 1):
+        logger.debug('Retrieving page %s of coingecko all coins list...', page)
 
-#         -- don't attempt coins that already have metadata
-#         and cgm.coingecko_id is null
-#         group by 1,2,3
-#         '''
+        # retrieve the metadata
+        coingecko_get_all_coins_page(page,batch_datetime)
 
-#     update_queue_df = dgc().run_sql(query_sql)
-#     logger.info('coins to update: %s', str(update_queue_df.shape[0]))
+        # rate limit pause
+        logger.info('pausing 15 seconds to avoid coingecko api rate limit issues...')
+        time.sleep(15)
 
-#     # ping api for each coin
-#     for i in range(len(update_queue_df)):
-#         blockchain = update_queue_df.iloc[i]['chain_text_coingecko']
-#         address = update_queue_df.iloc[i]['address']
-#         coin_id = update_queue_df.iloc[i]['coin_id']
-
-#         logger.info('initiating coingecko metadata search for <%s:%s>', blockchain, address)
-#         coingecko_metadata_search(
-#                 blockchain
-#                 ,address
-#                 ,coin_id
-#             )
-
-#         # rate limit pause
-#         logger.info('pausing 15 seconds to avoid coingecko api rate limit issues...')
-#         time.sleep(15)
-
-#     return "coingecko metadata update completed."
+    return "coingecko metadata update completed."
 
 
 
-# def coingecko_metadata_search(blockchain, address, coin_id):
-#     '''
-#     For a given blockchain and address, attempts to look up the coin on Coingecko by calling
-#     fetch_coingecko_data(). If the search is successful, stores the metadata in GCS.
+def coingecko_get_all_coins_page(page,batch_datetime):
+    '''
+    Retrieves a page of 250 coins from the coingecko all coins endpoint
 
-#     param: blockchain <string> this must match chain_text_coingecko from core.chains
-#     param: address <string> token contract address
-#     param: coin_id <dataframe> core.coins.coin_id which is added to bigquery records
-#     '''
-#     # get GCP credentials
-#     credentials = dgc().credentials
+    param: page <int> the page number of the all coins list to retrieve
+    param: batch_datetime <string> a string representation of when this batch of ids was retrieved
+    '''
+    # get GCP credentials
+    credentials = dgc().credentials
 
-#     # making the api call
-#     response_data = fetch_coingecko_data(blockchain, address)
+    # making the api call
+    response_data = fetch_coingecko_data(page)
 
-#     try:
-#         coingecko_id = response_data['id']
-#         search_successful = True
-#         search_log = 'search successful'
-#         logger.info('search successful for <%s:%s>', blockchain, address)
-#     except KeyError:
-#         coingecko_id = None
-#         search_successful = False
-#         search_log = 'KeyError: ID not found in response data'
-#         logger.info('FAILURE: KeyError - search failed for <%s:%s>', blockchain, address)
-#     except (TypeError, AttributeError):
-#         coingecko_id = None
-#         search_successful = False
-#         search_log = 'TypeError or AttributeError: Invalid response data'
-#         logger.info('FAILURE: TypeError or AttributeError - search failed for <%s:%s>'
-#                     , blockchain, address)
+    # store the data in gcs if the response was correctly formed
+    if 'id' in response_data[0]:
+        # storing json in gcs
+        filepath = 'data_lake/coingecko_all_coins/'
+        filename = f'all_coins_{batch_datetime}_page_{page}'
 
-#     # storing json in gcs
-#     if search_successful:
-#         filepath = 'data_lake/coingecko_coin_metadata/'
-#         filename = str(response_data['id'] + '.json')
+        client = storage.Client(credentials=credentials, project='dreams-labs-data')
+        bucket = client.get_bucket('dreams-labs-storage')
 
-#         client = storage.Client(credentials=credentials, project='dreams-labs-data')
-#         bucket = client.get_bucket('dreams-labs-storage')
+        blob = bucket.blob(filepath + filename)
+        blob.upload_from_string(json.dumps(response_data), content_type='json')
 
-#         blob = bucket.blob(filepath + filename)
-#         blob.upload_from_string(json.dumps(response_data), content_type='json')
+        logger.debug('%s uploaded successfully', filename)
 
-#         logger.info('%s uploaded successfully', filename)
-
-#     # store search result in etl_pipelines.coin_coingecko_ids
-#     client = bigquery.Client(credentials=credentials, project='dreams-labs-data')
-#     table_id = 'western-verve-411004.etl_pipelines.coin_coingecko_ids'
-
-#     rows_to_insert = [{
-#         'coin_id': coin_id,
-#         'coingecko_id': coingecko_id,
-#         'search_successful': search_successful,
-#         'search_date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-#         'search_log': search_log
-#     }]
-
-#     errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
-#     if not errors:
-#         logger.info("new row added to etl_pipelines.coin_coingecko_ids")
-#     else:
-#         logger.info("Encountered errors while inserting rows: %s", errors)
+        # upload the data to bigquery
+        upload_coingecko_data_to_bigquery(response_data, batch_datetime)
 
 
 
-def fetch_coingecko_data(blockchain, address, max_retries=3, retry_delay=30):
+
+def upload_coingecko_data_to_bigquery(json_data: List[Dict], batch_datetime) -> None:
+    """
+    Extracts relevant fields from a list of coin data in JSON format and uploads them to BigQuery.
+
+    Args:
+        json_data (List[Dict]): List of dictionaries, each representing data for a single coin.
+        batch_datetime <string> a string representation of when this batch of ids was retrieved
+
+    Returns:
+        None: Prints the status of the upload (success or error).
+    """
+    # Initialize BigQuery client
+    client = bigquery.Client()
+    table_id = 'western-verve-411004.etl_pipelines.coingecko_all_coins'
+
+    # define the created_at var with the correct formatting
+    parsed_date = datetime.datetime.strptime(batch_datetime, '%Y_%m_%d__%H_%M_%S')
+    created_at = parsed_date.strftime('%Y-%m-%d %H:%M:%S')
+
+
+    # Prepare rows for BigQuery
+    rows_to_insert = []
+    for coin in json_data:
+        row = {
+            "id": coin["id"],
+            "symbol": coin["symbol"],
+            "name": coin["name"],
+            "current_price": coin.get("current_price"),
+            "market_cap": coin.get("market_cap"),
+            "market_cap_rank": coin.get("market_cap_rank"),
+            "fully_diluted_valuation": coin.get("fully_diluted_valuation"),
+            "total_volume": coin.get("total_volume"),
+            "ath_date": coin.get("ath_date").replace("Z", "") if coin.get("ath_date") else None,
+            "atl_date": coin.get("atl_date").replace("Z", "") if coin.get("atl_date") else None,
+            "last_updated_coingecko": coin.get("last_updated").replace("Z", "") if coin.get("last_updated") else None,  # pylint: disable=C0301
+            "created_at": created_at
+        }
+        rows_to_insert.append(row)
+
+    # Insert rows into BigQuery table
+    errors = client.insert_rows_json(table_id, rows_to_insert)
+
+    if not errors:
+        logger.debug("Rows successfully inserted.")
+    else:
+        logger.warning("Encountered errors: %s", errors)
+
+
+
+def fetch_coingecko_data(page, max_retries=3, retry_delay=30):
     '''
     Makes an API call to Coingecko and returns the response data.
     Retries the call if a rate limit error (429) is encountered.
 
-    param: blockchain <string> this must match chain_text_coingecko from core.chains
-    param: address <string> token contract address
+    param: page <int> the page number of the all coins list to retrieve
     param: max_retries <int> number of times to retry on 429 error
     param: retry_delay <int> delay in seconds between retries
+
     returns: response_data <dict> JSON response data from Coingecko API
     '''
     coingecko_api_key = os.getenv('COINGECKO_API_KEY')
     headers = {'x_cg_pro_api_key': coingecko_api_key}
-    url = f'https://api.coingecko.com/api/v3/coins/{blockchain}/contract/{address}'
+    url = f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page={page}'
 
     for attempt in range(max_retries):
         response = requests.get(url, headers=headers, timeout=30)
@@ -156,7 +146,7 @@ def fetch_coingecko_data(blockchain, address, max_retries=3, retry_delay=30):
 
         if 'status' in response_data and response_data['status'].get('error_code') == 429:
             logger.info("Rate limit exceeded, retrying in %d seconds... (Attempt %d of %d)",
-                         retry_delay, attempt + 1, max_retries)
+                            retry_delay, attempt + 1, max_retries)
             time.sleep(retry_delay)
         else:
             return response_data
