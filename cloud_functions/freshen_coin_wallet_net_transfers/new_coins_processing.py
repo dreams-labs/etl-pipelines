a9d7cbde4d9c97ede12278ee52fd75288433331c
tentabs00 <tentabs00@gmail.com>
2024-12-04 17:41:59 -0800

    pipeline to return transfer counts to bigquery is working

---
+++ cloud_functions/freshen_coin_wallet_net_transfers/new_coins_processing.py
@@ -0,0 +1,337 @@
+"""
+provides updated whale chart data by following this sequence:
+1. updates the dune table net_transfers_state with the current state of the bigquery table \
+    etl_pipelines.coin_wallet_net_transfers
+2. generates a dune query for all blockchains in need of updates and unions them together
+3. retrieves the dune results and uploads them to bigquery
+
+"""
+import datetime
+import os
+import json
+import pandas as pd
+from dune_client.client import DuneClient
+from dune_client.query import QueryBase
+import pandas_gbq
+import functions_framework
+from dreams_core.googlecloud import GoogleCloud as dgc
+from dreams_core import core as dc
+
+# set up logger at the module level
+logger = dc.setup_logger()
+
+
+def load_new_coins_to_dune(dune_chains):
+    """
+    updates the dune table etl_net_transfers_freshness with all coins that don't have any
+    dune transfer records.
+
+    Params:
+    - dune_chains (list): a list that includes chain names that match Dune's terminology
+    """
+    # retrieve freshness df
+    dune_chains_string = "','".join(dune_chains)
+    query_sql = f"""
+        with existing_records as (
+            select chain_text_source as chain
+            ,token_address
+            ,decimals
+            ,max(date) as freshest_date
+            from etl_pipelines.coin_wallet_net_transfers
+            where data_source = 'dune'
+
+            -- all ethereum transfers are sourced from the public ethereum transfers table
+            and chain_text_source <> 'ethereum'
+            group by 1,2,3
+        )
+        ,new_records as (
+            select ch.chain_text_dune as chain
+            ,c.address as token_address
+            ,c.decimals
+            ,cast('2000-01-01' as datetime) as freshest_date
+            from core.coins c
+            join core.chains ch on ch.chain_id = c.chain_id
+            join (
+                select coin_id
+                ,max(market_cap) as max_market_cap
+                from core.coin_market_data
+                group by 1
+            ) cap_size on cap_size.coin_id = c.coin_id
+            left join existing_records e on e.token_address = c.address
+                and e.chain = ch.chain_text_dune
+            left join etl_pipelines.core_transfers_coin_exclusions coin_exclusions on coin_exclusions.coin_id = c.coin_id
+
+            -- remove coin exclusions
+            where coin_exclusions.coin_id is null
+
+            -- all ethereum transfers are sourced from the public ethereum transfers table
+            and c.chain <> 'Ethereum'
+
+            -- new coins don't have existing transfer data
+            and e.token_address is null
+
+            -- new coins must have dune-supported blockchains
+            and ch.chain_text_dune is not null
+
+            -- new coins currently need decimal data to run the dune queries
+            and c.decimals is not null
+
+            -- max market cap is used to prioritize smaller coins with lower credit cost
+            order by cap_size.max_market_cap asc
+            -- limit 20
+        )
+        select chain
+        ,token_address
+        ,current_timestamp() as updated_at
+        from (
+            select * from new_records
+        )
+
+        -- all eth transfers are handled from bigquery public data
+        where chain in ('{dune_chains_string}')
+        and token_address not in (
+            'EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm'
+            ,'25hAyBQfoDhfWx9ay6rarbgvWGwDdNqcHsXS3jQ3mTDJ'
+            ,'FU1q8vJpZNUrmqsciSjp8bAKKidGsLmouB8CBdf8TKQv'
+            ,'5z3EqYQo9HiCEs3R84RCDMu2n7anpDMxRhdK8PSWmrRC'
+            ,'69kdRLyP5DTRkpHraaSZAQbWmAwzF9guKjZfzMXzcbAs'
+            ,'GtDZKAqvMZMnti46ZewMiXCa4oXF4bZxwQPoKzXPFxZn'
+            ,'52DfsNknorxogkjqecCTT3Vk2pUwZ3eMnsYKVm4z3yWy'
+            ,'BSHanq7NmdY6j8u5YE9A3SUygj1bhavFqb73vadspkL3'
+            ,'DcUoGUeNTLhhzyrcz49LE7z3MEFwca2N9uSw1xbVi1gm'
+            ,'5LafQUrVco6o7KMz42eqVEJ9LW31StPyGjeeu5sKoMtA'
+        )
+        order by chain,token_address
+    """
+    new_coins_df = dgc().run_sql(query_sql)
+    logger.info('Retrieved metadata for %s tokens with no transfers data.', len(new_coins_df))
+
+    # store df locally as csv
+    local_csv = 'new_erc20_coins.csv'
+    new_coins_df.to_csv(local_csv, index=False)
+
+    # append the csv to dune
+    dune = DuneClient.from_env()
+    with open(local_csv, "rb") as data:
+        response = dune.insert_table(
+                    namespace='dreamslabs',
+                    table_name='etl_new_erc20_coins',
+                    data=data,
+                    content_type='text/csv'
+                )
+    logger.info('Dune append outcome: <%s>', response)
+
+    # remove local csv
+    os.remove(local_csv)
+
+    return new_coins_df
+
+
+
+def generate_erc_20_counts_query(dune_chains):
+    """
+    Generates a query with counts of how many transfer records need to be ingested
+    for all the coins of the input dune chains.
+
+    params:
+        dune_chains <set>: a set of all blockchains that need freshness updates
+
+    returns:
+        full_query <str>: the long dune query that will generates the wallet-coin-day-transfers
+    """
+
+    # all erc20 tokens have identical table structures so this query can be repeated for each
+    def erc20_query(chain_text_dune):
+        return f"""
+        {chain_text_dune} as (
+            --  retrieving the most recent batch of bigquery records available in dune
+            with new_coin_batch as (
+                select chain
+                ,token_address
+                ,updated_at
+                from (
+                    select *
+                    ,rank() over (order by updated_at desc) as batch_recency
+                    from dune.dreamslabs.etl_new_erc20_coins t
+                )
+                where batch_recency = 1
+            ),
+
+            -- filter transfers on index column 'block_time' to improve performance
+            transfers_filtered as (
+                select '{chain_text_dune}' as chain
+                ,t.evt_block_time as block_time
+                ,cast(t."from" as varchar) as from_token_account
+                ,cast(t."to" as varchar) as to_token_account
+                ,cast(t.contract_address as varchar) as token_mint_address
+                from erc20_{chain_text_dune}.evt_Transfer t
+                -- remove rows from today since the daily net totals aren't finalized
+                where date_trunc('day', t.evt_block_time at time zone 'UTC') <
+                    date(current_timestamp at time zone 'UTC')
+            ),
+            transfers as (
+                select t.chain
+                ,date_trunc('day', t.block_time at time zone 'UTC') as date
+                ,t.from_token_account as address
+                ,token_mint_address as contract_address
+                from transfers_filtered t
+                join new_coin_batch ts
+                    on ts.token_address = t.token_mint_address
+                    and ts.chain = t.chain
+
+                union all
+
+                select t.chain
+                ,date_trunc('day', t.block_time at time zone 'UTC') as date
+                ,t.to_token_account as address
+                ,token_mint_address as contract_address
+                from transfers_filtered t
+                join new_coin_batch ts
+                    on ts.token_address = t.token_mint_address
+                    and ts.chain = t.chain
+            ),
+            daily_net_transfers as (
+                select chain
+                ,date
+                ,address
+                ,contract_address
+                from transfers
+                group by 1,2,3,4
+            )
+
+            select chain
+            ,contract_address
+            ,count(date) as transfer_records
+            from daily_net_transfers
+            group by 1,2
+            order by 3 asc
+        )
+        """
+    # Define strings that will become queries
+    query_ctes = None
+    query_selects = None
+
+    # ERC20 blockchain query logic
+    for chain_text_dune in dune_chains:
+
+        # Solana is not ERC20
+        if chain_text_dune=='solana':
+            continue
+
+        # The first queries shouldn't have a UNION appended to them
+        if not query_ctes:
+            query_ctes = f'with {erc20_query(chain_text_dune)}'
+            query_selects = f'select * from {chain_text_dune}'
+            continue
+
+        # Join all queries together to be run at once
+        query_ctes = '\n,'.join([query_ctes,erc20_query(chain_text_dune)])
+        query_selects = '\nunion all\n'.join([query_selects,f'select * from {chain_text_dune}'])
+
+    full_query = query_ctes+query_selects
+    logger.info('generated full transfer count query.')
+
+    return full_query
+
+
+def get_dune_transfer_counts(new_coins_query):
+    """
+    runs the query in dune and retrieves the results as a df.
+
+    params:
+        full_query (str): sql query to run
+    returns:
+        new_coins_df (json): df showing how many transfers records exist for the coins
+    """
+    dune = DuneClient.from_env()
+
+    # Update the query with a version that includes all necessary blockchains
+    query_id = dune.update_query(
+        query_id = 4375482,
+        query_sql=new_coins_query
+    )
+
+    transfers_query = QueryBase(
+        query_id=query_id,
+    )
+    # Run Dune query and load to a dataframe
+    logger.info('fetching fresh dune data...')
+    transfer_counts_df = dune.run_query_dataframe(
+        transfers_query,
+        performance='large',
+        ping_frequency=10
+        )
+    logger.info('Retrieved transfer counts for %s coins.', len(transfer_counts_df))
+
+    return transfer_counts_df
+
+
+
+def upload_transfer_counts(transfer_counts_df):
+    """
+    Appends the Dune query results to BigQuery table etl_pipelines.dune_new_coin_transfer_counts.
+
+    Params:
+    - transfer_counts_df (pd.DataFrame): result from Dune query with columns
+        chain,contract_address,transfer_records
+    """
+    # copy df and add updated_at column
+    upload_df = transfer_counts_df.copy()
+    upload_df['updated_at'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # set df datatypes of upload df
+    dtype_mapping = {
+        'chain': str,
+        'contract_address': str,
+        'transfer_records': int,
+        'updated_at': 'datetime64[ns, UTC]'
+    }
+    upload_df = upload_df.astype(dtype_mapping)
+    logger.info('Prepared upload df with %s rows.',len(upload_df))
+
+    # upload df to bigquery
+    project_id = 'western-verve-411004'
+    table_name = 'etl_pipelines.dune_new_coin_transfer_counts'
+    schema = [
+        {'name':'chain', 'type': 'string'},
+        {'name':'contract_address', 'type': 'string'},
+        {'name':'transfer_records', 'type': 'int64'},
+        {'name':'updated_at', 'type': 'datetime'}
+    ]
+    pandas_gbq.to_gbq(
+        upload_df
+        ,table_name
+        ,project_id=project_id
+        ,if_exists='append'
+        ,table_schema=schema
+        ,progress_bar=False
+    )
+    logger.info('Updated etl_pipelines.dune_new_coin_transfer_counts with new counts.')
+
+
+
+# def create_dune_new_coins_table():
+#     """
+#     this is the code that was used to create dune.dreamslabs.etl_new_erc20_coins.
+#     it is not intended to be reran as part of normal operations but is retained in case it needs
+#     to be referenced or altered.
+
+#     params:
+#         none
+#     returns:
+#         none
+#     """
+#     # make empty dune table
+#     dune = DuneClient.from_env()
+
+#     table = dune.create_table(
+#         namespace='dreamslabs',
+#         table_name='etl_new_erc20_coins',
+#         description='erc20 coins that do not have dune transfer data',
+#         schema= [
+#             {'name': 'chain', 'type': 'varchar'},
+#             {'name': 'token_address', 'type': 'varchar'},
+#             {'name': 'updated_at', 'type': 'timestamp'},
+#         ],
+#         is_private=False
+#     )
