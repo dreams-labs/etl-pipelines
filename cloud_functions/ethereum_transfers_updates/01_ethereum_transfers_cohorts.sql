2dd7881faab7d8551ade5bf6541eaa079d96b9c3
tentabs00 <tentabs00@gmail.com>
2025-01-03 01:44:54 -0800

    cohort 5

--- cloud_functions/ethereum_transfers_updates/01_ethereum_transfers_cohorts.sql
+++ cloud_functions/ethereum_transfers_updates/01_ethereum_transfers_cohorts.sql
@@ -123,3 +123,39 @@ insert into etl_pipelines.ethereum_transfers_cohorts (
     and excl.coin_id is null
 
 )
+
+
+-- COHORT 5: 1009 coins
+insert into etl_pipelines.ethereum_transfers_cohorts (
+
+    select 5 as cohort_number
+    ,'coingecko_all_coins rank up to 14000 as of jan 2 2025' as cohort_description
+    ,c.coin_id
+    ,c.coingecko_id
+    ,c.chain
+    ,c.address
+    ,current_datetime("UTC") as created_at
+    ,c.decimals
+    from core.coins c
+    left join etl_pipelines.ethereum_transfers_cohorts etc on etc.coin_id = c.coin_id
+    left join (
+        select token_address
+        from etl_pipelines.ethereum_net_transfers t
+        group by 1
+    ) t on t.token_address = c.address
+    left join etl_pipelines.ethereum_transfers_exclusions excl on excl.coin_id = c.coin_id
+
+    -- only include coins on eth
+    where c.chain = 'Ethereum'
+
+    -- decimals are needed in the etl pipelines
+    and c.decimals is not null
+
+    -- only include coins that aren't already in the eth transfers table or in a cohort
+    and etc.coin_id is null
+    and t.token_address is null
+
+    -- exclude the exclusion list
+    and excl.coin_id is null
+
+)
